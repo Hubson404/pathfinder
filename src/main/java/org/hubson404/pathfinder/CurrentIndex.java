299ea6199aae0da782fcea97b8a4b7ce02ac8768
Hubson404 <60826441+Hubson404@users.noreply.github.com>
2020-09-18 21:16:02 +0200

    Add CurrentIndex class

---
+++ src/main/java/org/hubson404/pathfinder/CurrentIndex.java
@@ -0,0 +1,19 @@
+package org.hubson404.pathfinder;
+
+public class CurrentIndex {
+
+    private final int initialRow;
+    private final int initialColumn;
+    private int row;
+    private int column;
+    private Field currentField;
+    private final Field[][] board;
+
+    public CurrentIndex(int row, int column, Field[][] board) {
+        this.row = row;
+        this.column = column;
+        this.initialRow = row;
+        this.initialColumn = column;
+        this.board = board;
+    }
+}
