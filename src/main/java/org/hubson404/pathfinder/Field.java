f6335a38ab907c0009be69654fe0e5e5e13cd041
Hubson404 <60826441+Hubson404@users.noreply.github.com>
2020-09-18 21:33:28 +0200

    Add methods checkNext...Field() Field class

--- src/main/java/org/hubson404/pathfinder/Field.java
+++ src/main/java/org/hubson404/pathfinder/Field.java
@@ -17,6 +17,22 @@ public class Field {
         this.fieldType = fieldType;
     }
 
+    public void checkNextRightField(Field[][] board) {
+        isRightEmpty = checkAdjecentField(DirectionType.RIGHT, board);
+    }
+
+    public void checkNextLeftField(Field[][] board) {
+        isLeftEmpty = checkAdjecentField(DirectionType.LEFT, board);
+    }
+
+    public void checkNextTopField(Field[][] board) {
+        isTopEmpty = checkAdjecentField(DirectionType.UP, board);
+    }
+
+    public void checkNextBottomField(Field[][] board) {
+        isBottomEmpty = checkAdjecentField(DirectionType.DOWN, board);
+    }
+
     private boolean checkAdjecentField(DirectionType direction, Field[][] board) {
         int nextRow = row;
         int nextColumn = column;
