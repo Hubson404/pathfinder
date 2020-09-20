acc2ead6e457f20d3b761615812bb176667e8865
Hubson404 <60826441+Hubson404@users.noreply.github.com>
2020-09-20 16:37:30 +0200

    Add PathLog class

---
+++ src/main/java/org/hubson404/pathfinder/PathLog.java
@@ -0,0 +1,12 @@
+package org.hubson404.pathfinder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PathLog {
+
+    public List<String> path = new ArrayList<>();
+
+    public PathLog() {
+    }
+}
